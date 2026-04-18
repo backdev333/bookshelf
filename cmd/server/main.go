5e5cf83664683dc26d5d9940179ea2c0a71b1770
Vasil Kamaev <vasil.kamaev@yandex.ru>
2026-04-18 21:07:33 +0300

    Минимальный HTTP-сервер

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,7 +1,36 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"log/slog"
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, Bookshelf!")
+	port := ":8080"
+
+	http.HandleFunc("/health", healthHandler)
+
+	slog.Info("Server starting", "port", port)
+	http.ListenAndServe(port, nil)
+}
+
+func healthHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		w.Write([]byte("method not supported. use GET."))
+		return
+	}
+
+	bytes, err := json.MarshalIndent(struct {
+		Status string `json:"status"`
+	}{"ok"}, "", "	")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		slog.Error("json marshal", "error", err)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write(bytes)
 }
