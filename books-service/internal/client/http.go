c0d59dacc8c21ab1c171cac80637ebc07674010f
Vasil Kamaev <vasil.kamaev@yandex.ru>
2026-06-25 12:03:31 +0300

    Add HTTPClient: implement GET and POST methods for HTTP requests

--- books-service/internal/client/http.go
+++ books-service/internal/client/http.go
@@ -1,1 +1,73 @@
 package client
+
+import (
+	"context"
+	"log/slog"
+	"net/http"
+	"net/url"
+	"time"
+)
+
+type HTTPClient struct {
+	Client  *http.Client `json:"client"`
+	BaseURL string       `json:"base_url"`
+}
+
+func NewHTTPClient(baseURL string, timeout time.Duration) *HTTPClient {
+	return &HTTPClient{
+		Client: &http.Client{
+			Timeout: timeout,
+		},
+		BaseURL: baseURL,
+	}
+}
+
+func (c *HTTPClient) Get(ctx context.Context, path string, headers map[string]string) (*http.Response, error) {
+	path, err := url.JoinPath(c.BaseURL, path)
+	if err != nil {
+		slog.Error("HTTPClient.Get() url.JoinPath", "error", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodGet, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		slog.Error("HTTPClient.Get() client.Do", "error", err)
+		return nil, err
+	}
+
+	return res, nil
+}
+
+func (c *HTTPClient) Post(ctx context.Context, path string, body interface{}, headers map[string]string) (*http.Response, error) {
+	path, err := url.JoinPath(c.BaseURL, path)
+	if err != nil {
+		slog.Error("HTTPClient.Post() url.JoinPath", "error", err)
+		return nil, err
+	}
+
+	req, err := http.NewRequestWithContext(ctx, http.MethodPost, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v)
+	}
+
+	res, err := c.Client.Do(req)
+	if err != nil {
+		slog.Error("HTTPClient.Get() client.Do", "error", err)
+		return nil, err
+	}
+
+	return res, nil
+}
